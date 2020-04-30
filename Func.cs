c54dbb53176979c6e443d5fd61cf985c8a088211
Аліна Шелест <Al.Shelest@ssu.edu.ua>
2020-04-30 12:04:54 +0300

    Добавьте файлы проекта.

---
+++ Func.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8_3
+{
+    class Func
+    {
+            public double Length(double r)
+            {
+            if (r >= 0)
+            {
+                return 2 * Math.PI * r;
+            }
+            else
+            {
+                throw new FormatException("Invalid input data");
+            }
+            }
+
+            public double Square(double r)
+            {
+            if (r >= 0)
+            {
+                return Math.PI * r * r;
+            }
+            else
+            {
+                throw new FormatException("Invalid input data");
+            }
+            }
+            public double Volume(double r)
+            {
+            if (r >= 0)
+            {
+                return (4.0 / 3.0) * Math.PI * r * r * r;
+            }
+            else
+            {
+                throw new FormatException("Invalid input data");
+            }
+            }
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Оголосити тип делегату, що посилається на метод. Вимоги до сигнатури методу наступні:
+ метод отримує вхідним параметром змінну типу double;
+ метод повертає значення типу double, яке є результатом обчислення згідно з умовою задачі.
+Реалізувати виклик трьох методів з допомогою одного делегату, які отримують радіус кола R в якості
+вхідного параметру і обчислюють:
+довжину кола за формулою D = 2πR;
+ площу круга за формулою S = πR2;\
+ об‘єм кулі. Формула V = 4/3πR3.
+Методи повинні бути представлені в окремому класі як нестатичні.*/
+namespace lab8_3
+{
+    class Program
+    {
+        delegate double Delegate(double p);
+        static void Main(string[] args)
+        {
+            double r, result;
+            Func functions = new Func();
+            Delegate delegat;
+            Console.Write("Enter R ");
+            r = Double.Parse(Console.ReadLine());
+
+            delegat = functions.Length;
+            try
+            {
+                result = delegat(r);
+                Console.WriteLine("Length = " + result);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format of input");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow");
+            }
+            catch (OutOfMemoryException)
+            {
+                Console.WriteLine("Out Of Memory to create new object");
+            }
+
+            delegat = functions.Square;
+            try
+            {
+                result = delegat(r);
+                Console.WriteLine("Square = " + result);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format of input");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow");
+            }
+            catch (OutOfMemoryException)
+            {
+                Console.WriteLine("Out Of Memory to create new object");
+            }
+
+            delegat = functions.Volume;
+            try
+            {
+                result = delegat(r);
+                Console.WriteLine("Volume = " + result);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format of input");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow");
+            }
+            catch (OutOfMemoryException)
+            {
+                Console.WriteLine("Out Of Memory to create new object");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
